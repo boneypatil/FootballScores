3a1de2ed4ea6cd3cac6a7345e0138f16b57a0191
boneypatil <rahulpatilbgm52@gmail.com>
2021-07-03 20:06:14 +0530

    component updated

--- app/src/main/java/com/module/footballscores/dagger/component/AppComponent.kt
+++ app/src/main/java/com/module/footballscores/dagger/component/AppComponent.kt
@@ -2,7 +2,6 @@ package com.module.footballscores.dagger.component
 
 import com.module.footballscores.dagger.module.NetworkModule
 import com.module.footballscores.dagger.module.viewModule.ViewModelModule
-import com.module.footballscores.ui.ActivityMatchResult
 import com.module.footballscores.ui.FragmentMatchResult
 import dagger.Component
 import javax.inject.Singleton
